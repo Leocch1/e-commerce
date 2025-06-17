e5c70ae7c66ddd57558c0da25452b504fa63b282
Leo Louise Jimenez <bc.leolouise.jimenez@cvsu.edu.ph>
2025-06-18 05:57:15 +0800

    Building the Success Page

--- actions/createCheckoutSession.ts
+++ actions/createCheckoutSession.ts
@@ -37,6 +37,13 @@ export async function createCheckoutSEssion(
             customerId = customers.data[0].id;
         }
 
+    const isProd = process.env.NODE_ENV === "production";
+    const baseUrl = isProd
+        ? `https://${process.env.VERCEL_URL}`
+        : process.env.NEXT_PUBLIC_BASE_URL;
+    const successUrl = `${baseUrl}/success?session_id={CHECKOUT_SESSION_ID}&orderNumber=${metadata.orderNumber}`;
+    const cancelUrl = `${baseUrl}/basket`;
+
     const session = await stripe.checkout.sessions.create({
         customer: customerId,
             customer_creation: customerId ? undefined : "always",
@@ -45,9 +52,8 @@ export async function createCheckoutSEssion(
             mode: "payment",
             allow_promotion_codes: true,
             payment_method_types: ['card'],
-            success_url: `${`https://${process.env.VERCEL_URL}` || process.env.NEXT_PUBLIC_BASE_URL}/success?session_id={CHECHOUT_SESSION_ID}
-            &orderNumber=${metadata.orderNumber}`,
-            cancel_url: `${`https://${process.env.VERCEL_URL}` || process.env.NEXT_PUBLIC_BASE_URL}/basket`,
+            success_url: successUrl,
+            cancel_url: cancelUrl,
             line_items: items.map((item) => ({
                 price_data: {
                     currency: "php",

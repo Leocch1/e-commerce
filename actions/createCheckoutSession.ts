65d54af3318b5426adb531d1e52c7cf79ac0389c
Leo Louise Jimenez <bc.leolouise.jimenez@cvsu.edu.ph>
2025-06-18 04:29:21 +0800

    Implementing Stripe for Payment Processing

--- actions/createCheckoutSession.ts
+++ actions/createCheckoutSession.ts
@@ -1,5 +1,7 @@
 'use server'
 
+import { imageUrl } from "@/lib/imageUrl";
+import stripe from "@/lib/stripe";
 import { BasketItem } from "@/store/store";
 
 export type Metadata = {
@@ -23,7 +25,49 @@ export async function createCheckoutSEssion(
         if(itemsWithoutPrice.length > 0) {
             throw new Error("Some items do not have a price")
         }
-        
+
+        //Search for existing customer by email
+        const customers = await stripe.customers.list({
+            email: metadata.customerEmail,
+            limit: 1,
+        })
+
+        let customerId: string | undefined;
+        if (customers.data.length > 0) {
+            customerId = customers.data[0].id;
+        }
+
+    const session = await stripe.checkout.sessions.create({
+        customer: customerId,
+            customer_creation: customerId ? undefined : "always",
+            customer_email: !customerId ? metadata.customerEmail : undefined,
+            metadata,
+            mode: "payment",
+            allow_promotion_codes: true,
+            payment_method_types: ['card'],
+            success_url: `${`https://${process.env.VERCEL_URL}` || process.env.NEXT_PUBLIC_BASE_URL}/success?session_id={CHECHOUT_SESSION_ID}
+            &orderNumber=${metadata.orderNumber}`,
+            cancel_url: `${`https://${process.env.VERCEL_URL}` || process.env.NEXT_PUBLIC_BASE_URL}/basket`,
+            line_items: items.map((item) => ({
+                price_data: {
+                    currency: "php",
+                    unit_amount: Math.round(item.product.price! * 100),
+                    product_data: {
+                        name: item.product.name || "Unnamed Product",
+                        description: `Product ID: ${item.product._id}`,
+                        metadata: {
+                            id: item.product._id
+                        },
+                        images: item.product.image
+                            ? [imageUrl(item.product.image).url()]
+                            :undefined
+                    }
+                },
+                quantity: item.quantity
+            }))
+    })
+
+    return session.url;    
     } catch (error) {
         console.error("Error creating checkout session:", error)
         throw error;

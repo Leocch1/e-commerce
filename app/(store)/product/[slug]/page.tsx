bafdefc56a73ef6ac45817d2354f37f32aacdff0
Leo Louise Jimenez <bc.leolouise.jimenez@cvsu.edu.ph>
2025-06-25 08:02:50 +0800

    product page

--- app/(store)/product/[slug]/page.tsx
+++ app/(store)/product/[slug]/page.tsx
@@ -1,21 +1,12 @@
-// app/(store)/product/[slug]/page.tsx
-
 import { getProductBySlug } from "@/sanity/lib/products/getProductBySlug";
 import { notFound } from "next/navigation";
 import ProductClientView from "@/components/ProductClientView";
 
-type Props = {
-  params: {
-    slug: string;
-  };
-};
-
+export const dynamic = "force-static";
 export const revalidate = 60;
 
-export default async function ProductPage({ params }: Props) {
-  const { slug } = params;
-
-  const product = await getProductBySlug(slug);
+export default async function Page({ params }: { params: { slug: string } }) {
+  const product = await getProductBySlug(params.slug);
 
   if (!product) return notFound();
 

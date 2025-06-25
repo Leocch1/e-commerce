e989ae003fcc3aff903c859d2afe4083c01b9376
Leo Louise Jimenez <bc.leolouise.jimenez@cvsu.edu.ph>
2025-06-25 08:20:47 +0800

    fixed product params

--- app/(store)/product/[slug]/page.tsx
+++ app/(store)/product/[slug]/page.tsx
@@ -5,13 +5,11 @@ import ProductClientView from "@/components/ProductClientView";
 export const dynamic = "force-static";
 export const revalidate = 60;
 
-type ProductPageProps = {
-  params: {
-    slug: string;
-  };
+type PageProps = {
+  params: { slug: string };
 };
 
-export default async function Page({ params }: ProductPageProps) {
+export default async function Page({ params }: PageProps) {
   const product = await getProductBySlug(params.slug);
 
   if (!product) return notFound();
